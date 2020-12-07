0815f9ae41f18c0c0fecfbc78f348d2d1ebcd8d0
xrystmaz <72700262+xrystmaz@users.noreply.github.com>
2020-12-07 11:31:29 +0300

    Update domashka 71220.py

--- domashka 71220.py
+++ domashka 71220.py
@@ -14,16 +14,3 @@ def NOK(a, b):
     a = a * b // t
     print("НОК:", a)
 NOK(a, b)
-number = int(input())
-number2 = number
-factors = []
-d = 2
-def Primenumberdecomposition(number, d):
-    for i in range(2, int(math.sqrt(number)) + 1):
-        while number % i == 0:
-            factors.append(i)
-            number //= i
-    if number != 1:
-        factors.append(number)
-    print('{} = {}'.format(number2, factors))
-t = Primenumberdecomposition(number, d)
